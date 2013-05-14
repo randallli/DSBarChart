57d340a8326641b0a6d69176348ed4309afe12a3
Randall Li <Randall@tripleli.com>
2013-05-14 01:02:46 -0400

    added second chart

--- DSBarChart/DSViewController.m
+++ DSBarChart/DSViewController.m
@@ -20,23 +20,49 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSArray *vals = [NSArray arrayWithObjects:
-                    [NSNumber numberWithInt:30],
-                    [NSNumber numberWithInt:40],
-                    [NSNumber numberWithInt:20],
-                    [NSNumber numberWithInt:56],
-                    [NSNumber numberWithInt:70],
-                    [NSNumber numberWithInt:34],
-                    [NSNumber numberWithInt:43],
-                    nil];
-    NSArray *refs = [NSArray arrayWithObjects:@"M", @"Tu", @"W", @"Th", @"F", @"Sa", @"Su", nil];
-    DSBarChart *chrt = [[DSBarChart alloc] initWithFrame:ChartView.bounds
-                                                   color:[UIColor greenColor]
-                                              references:refs
-                                               andValues:vals];
-    chrt.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    chrt.bounds = ChartView.bounds;
-    [ChartView addSubview:chrt];
+    NSInteger maxValue = 0;
+    {
+        NSArray *vals = [NSArray arrayWithObjects:
+                         [NSNumber numberWithInt:30],
+                         [NSNumber numberWithInt:40],
+                         [NSNumber numberWithInt:20],
+                         [NSNumber numberWithInt:56],
+                         [NSNumber numberWithInt:90],
+                         [NSNumber numberWithInt:34],
+                         [NSNumber numberWithInt:43],
+                         nil];
+        NSArray *refs = [NSArray arrayWithObjects:@"M", @"Tu", @"W", @"Th", @"F", @"Sa", @"Su", nil];
+        DSBarChart *chrt = [[DSBarChart alloc] initWithFrame:ChartView.bounds
+                                                       color:[UIColor greenColor]
+                                                  references:refs
+                                                   andValues:vals];
+        chrt.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        [ChartView addSubview:chrt];
+        maxValue = chrt.maxLen;
+    }
+    
+    {
+        NSArray *vals = [NSArray arrayWithObjects:
+                         [NSNumber numberWithInt:3],
+                         [NSNumber numberWithInt:4],
+                         [NSNumber numberWithInt:2],
+                         [NSNumber numberWithInt:5],
+                         [NSNumber numberWithInt:60],
+                         [NSNumber numberWithInt:3],
+                         [NSNumber numberWithInt:4],
+                         nil];
+        NSArray *refs = [NSArray arrayWithObjects:@"M", @"Tu", @"W", @"Th", @"F", @"Sa", @"Su", nil];
+        DSBarChart *chrt = [[DSBarChart alloc] initWithFrame:ChartView.bounds
+                                                       color:[UIColor orangeColor]
+                                                  references:refs
+                                                   andValues:vals];
+        chrt.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        chrt.opaque = NO;
+        chrt.shouldCalculateMaxValue = NO;
+        chrt.maxLen = 90;
+        [ChartView addSubview:chrt];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
